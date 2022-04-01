d4c8bdebe042632b74134f1406ff9ece84bb295e
Goku <vierosprojumpercs@mail.ru>
2022-04-02 01:24:12 +0500

    1 lesson dz

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Message } from "./components/Message";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="container">
+			<Message title={"Сообщение"} />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Message.js
@@ -0,0 +1,5 @@
+export const Message = ({title}) => {
+    return (
+        <div>{title}</div>
+    )
+}
